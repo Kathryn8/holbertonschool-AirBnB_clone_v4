59aff7f867b6da8f3fdaecf7ed8be7367ae04b71
Kathryn6368 <6368@holbertonstudents.com>
2023-09-26 19:55:45 +1000

    fixed html block to correctly display dynamic content!

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -41,13 +41,12 @@ $(document).ready(function () {
         $('section.places').append(`<article>
 				<div class="title_box">
 					<h2>${place.name}</h2>
-					<div class="price_by_night">${place.price_by_night}</div>
+					<div class="price_by_night">$${place.price_by_night}</div>
 				</div>
 				<div class="information">
-					<div class="max_guest">${place.max_guest} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-					<div class="number_rooms">${place.number_rooms} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-					<div class="number_bathrooms">${place.number_bathrooms} Bathroom{% if place.number_bathrooms != 1 %}s{%
-						endif %}</div>
+					<div class="max_guest">${place.max_guest} Guest${(place.max_guest != 1 ? "s" : "")}</div>
+					<div class="number_rooms">${place.number_rooms} Bedroom${(place.number_rooms != 1 ? "s" : "")}</div>
+					<div class="number_bathrooms">${place.number_bathrooms} Bathroom${(place.number_bathrooms != 1 ? "s" : "")}</div>
 				</div>
 				<div class="user">
 				</div>
